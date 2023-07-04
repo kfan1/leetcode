b3fffa28d6bd976e9becf5c3e8273d389a3964b5
Kevin Fan <kevinlifan@yahoo.com>
2023-07-04 00:51:26 -0400

    updated q1

--- q1.js
+++ q1.js
@@ -6,7 +6,7 @@
 var twoSum = function (nums, target) {
   let obj = {};
   for (let i = 0; i < nums.length; i++) {
-    if (obj[target - nums[i]] !== undefined) return [obj[target - nums[i]], i];
-    else obj[nums[i]] = i;
+    if (obj[target - nums[i]]) return [obj[target - nums[i]] - 1, i];
+    else obj[nums[i]] = i + 1;
   }
 };
