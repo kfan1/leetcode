c29360757ec1e4f08826c09e586858a3364a5d51
Kevin Fan <kevinlifan@yahoo.com>
2023-07-05 13:57:26 -0400

    q219

--- q219.js
+++ q219.js
@@ -3,6 +3,14 @@
  * @param {number} k
  * @return {boolean}
  */
-var containsNearbyDuplicate = function(nums, k) {
-    
+var containsNearbyDuplicate = function (nums, k) {
+  let numIndexMap = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (numIndexMap[nums[i]] !== undefined) {
+      if (i - numIndexMap[nums[i]] <= k) return true;
+    }
+    numIndexMap[nums[i]] = i;
+  }
+
+  return false;
 };
