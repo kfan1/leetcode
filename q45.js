31bcbe48fec1e977313a510ce0d4961b0b93d635
Kevin Fan <kevinlifan@yahoo.com>
2023-07-04 21:50:22 -0400

    q45

--- q45.js
+++ q45.js
@@ -3,14 +3,15 @@
  * @return {number}
  */
 var jump = function (nums) {
+  if (nums.length === 1) return 0;
+  
   let tracker = [0];
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = 0; j < nums[i]; j++) {
       if (tracker[i + j + 1] === undefined) {
         tracker[i + j + 1] = tracker[i] + 1;
+        if (tracker.length === nums.length) return tracker[nums.length - 1];
       }
     }
   }
-
-  return tracker[nums.length - 1];
 };
