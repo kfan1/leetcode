a612ed1ee466dfe14c5128b08bcfa69c486163bd
Kevin Fan <kevinlifan@yahoo.com>
2023-07-05 14:25:55 -0400

    q530

---
+++ q530.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var getMinimumDifference = function (root) {
+  let valArr = [];
+  (function traverse(root) {
+    if (!root) return;
+    traverse(root.left);
+    valArr.push(root.val);
+    traverse(root.right);
+  })(root);
+
+  let min = Infinity;
+
+  for (let i = 0; i < valArr.length - 1; i++) {
+    min = Math.min(valArr[i + 1] - valArr[i], min);
+  }
+
+  return min;
+};
