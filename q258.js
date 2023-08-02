d27c798bf2a94f5cd457845b89a9dd0baf8070b1
Kevin Fan <kevinlifan@yahoo.com>
2023-08-02 13:45:28 -0400

    completed q258

--- q258.js
+++ q258.js
@@ -2,6 +2,16 @@
  * @param {number} num
  * @return {number}
  */
-var addDigits = function(num) {
-    
+var addDigits = function (num) {
+  let newSum = num;
+  num = String(num);
+  while (num.length > 1) {
+    newSum = 0;
+    for (let i = 0; i < num.length; i++) {
+      newSum += Number(num[i]);
+    }
+    num = String(newSum);
+  }
+
+  return newSum;
 };
